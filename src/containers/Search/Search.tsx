bfb5539f20c6080cdaefdcfd376b66211ac30840
Anish Kargaonkar <anishkargaonkar@gmail.com>
2020-09-22 22:57:56 +0530

    add SearchInput component

--- src/containers/Search/Search.tsx
+++ src/containers/Search/Search.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from "react";
+import "./styles.css";
+import { SearchInput } from "./views/SearchInput";
 
 type Props = {};
 
 const Search: FC<Props> = (props: Props) => {
-    return (
-        <div>Search Container</div>
-    )
+  return (
+    <div className="container">
+      <SearchInput />
+    </div>
+  );
 };
 
 export { Search };
---
+++ src/containers/Search/views/SearchInput.tsx
@@ -0,0 +1,54 @@
+import React, { FC, useEffect, useState } from "react";
+import { Avatar, Input } from "antd";
+import logo from "../../../assets/logo.svg";
+import "../styles.css";
+import { useDispatch, useSelector } from "react-redux";
+import { ApplicationState } from "../../../store";
+import { getResultsRequest } from "../action";
+
+type Props = {};
+
+const { Search } = Input;
+const SearchInput: FC<Props> = (props: Props) => {
+  const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQueryLimit, setSearchQueryLimit] = useState(0);
+
+  const isLoading = useSelector<ApplicationState, boolean>(
+    (s) => s.search.isLoading
+  );
+
+  const onSearchQueryChanegeHandler = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const val = e.target.value;
+    setSearchQuery(val);
+  };
+
+  const onSearchHandler = () => {
+    dispatch(getResultsRequest({
+      query: searchQuery,
+      limit: searchQueryLimit
+    }))
+  }
+
+  useEffect(() => {
+    setSearchQueryLimit(25);
+  }, [])
+
+  return (
+    <div className="search-input-container">
+      <Avatar src={logo} shape="circle" size={150} />
+      <Search
+        className="search-input"
+        placeholder="Search for a topic"
+        loading={isLoading}
+        value={searchQuery}
+        onChange={onSearchQueryChanegeHandler}
+        onSearch={onSearchHandler}
+      />
+    </div>
+  );
+};
+
+export { SearchInput };
